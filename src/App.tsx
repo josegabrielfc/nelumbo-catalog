7da4ed2b83ae9d6fc19a5f319430ee441c654169
josegabrielfc <jgfchona@gmail.com>
2025-05-24 01:27:08 -0500

    feat: initialize Nelumbo Catalog project with React + Typescript, Vite, and TailwindCSS - Set up Vite configuration for React and Tailwind CSS (vite.config.ts)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <img src="./nelumbo.png" alt="nelumbo-logo" className="w-36 h-auto" />
+      <h2 className="text-4xl font-bold mb-4">Nelumbo Catalog</h2>
+    </div>
+  )
+}
+
+export default App
