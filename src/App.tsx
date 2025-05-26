ece147c68e7cb2ce7ed5535db2f63a391a570a80
josegabrielfc <jgfchona@gmail.com>
2025-05-26 00:36:01 -0500

    feat: add routing and main view with filtering and sorting components (UI only)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Navbar } from './components/ui/Navbar';
-import { Main } from './components/layout/main';
 import { Footer } from './components/ui/Footer';
+import Routes from './routes/Routes';
 
 function App() {
   return (
-    <div className="min-h-screen grid grid-rows-[auto_1fr]">
-      <Navbar />
-      <Main />
-      <div className="my-4"></div>
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen grid grid-rows-[auto_1fr]">
+        <Navbar />
+        <Routes />
+        <div className="my-4"></div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
