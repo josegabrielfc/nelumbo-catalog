76947daafea569f7eb3a298946cdf563a7f14724
josegabrielfc <106695664+josegabrielfc@users.noreply.github.com>
2025-05-26 00:31:20 -0500

    feat: add product data and Product interface with additional fields (#1)

---
+++ src/types/product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: string;
+  nombre: string;
+  marca: string;
+  precio: number;
+  precioOriginal?: number;
+  descuento?: number;
+  reviews: number; // escala de 1 a 5
+  categoria: string;
+  imagen: string;
+  memoria?: string;
+  ram?: string;
+  color?: string;
+}
